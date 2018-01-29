65c95ef63b0b88e3c4d0b971e9e5b2a4c703eddf
Kaiyang Zhao <zh_kaiyang@hotmail.com>
2018-01-29 08:33:22 -0500

    Update menu.ts

--- src/pages/menu/menu.ts
+++ src/pages/menu/menu.ts
@@ -10,23 +10,6 @@ export class MenuPage {
 
   xmlItems: any;
 
-  items = [
-    'Mega Man X',
-    'The Legend of Zelda',
-    'Pac-Man',
-    'Super Mario World',
-    'Street Fighter II',
-    'Half Life',
-    'Final Fantasy VII',
-    'Star Fox',
-    'Donkey Kong III',
-    'GoldenEye 007',
-    'Doom',
-    'Fallout',
-    'GTA',
-    'Halo'
-  ];
-
   itemSelected(item: string) {
     console.log("Selected Item", item);
   }
